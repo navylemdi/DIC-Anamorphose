5f277ae56ccf9d47f62a94d95ab128c063ebb496
navylemdi <84194324+navylemdi@users.noreply.github.com>
2021-10-19 18:42:43 -0400

    more comments

--- main.py
+++ main.py
@@ -1,36 +1,29 @@
 from Module import *
-import sys
-import numpy as np
 
 deck = Deck('/Users/yvan/Desktop/ETS_montreal/Cours/E21/MTR892 - Projet technique/AnamorphosePlane/deck.yaml')
 
 #Creation of the surface object
 S = Surface(deck.a, deck.b, deck.c, deck.Position, deck.radius, deck.SurfaceType)
-
+#Creation of the speckle
 speckle = Speckle(deck.NbImage, deck.PositionCentre, deck.Images(), deck.height, deck.width, deck.begining, deck.step)
-
+#Projection of the speckle
 Liste_Projection = speckle.ProjectionSpeckle(S)
-
+#Unfolding of the speckle
 List_Unfolded = speckle.UnfoldSpeckle(S)
-#Depliage = List_Unfolded[0]
 rotation_matrix = List_Unfolded[1]
 roulement_matrix = List_Unfolded[2]
 
-# print(np.shape(List_Unfolded))
-# print(np.shape(Depliage))
-# print(rotation_matrix)
-# print(roulement_matrix)
-
+#Unfolding of the wingframe and meshing of the anamorphosed speckle for print
 WingFrameUnfolded, yf, zf = Fonction.Unfold_object_frame(deck.WingFrame, S.SurfaceType, S.Gradient(), rotation_matrix, roulement_matrix, deck.widthPrintable, deck.heightPrintable)
 
 ##--------------------------------AFFICHAGE----------------------------------##
 
 p=Plot()
-p.PlotReference(deck.NbImage, speckle.List_Sheets)
+p.PlotReference(deck.NbImage, speckle.List_Sheets)#Plot the loaded speckle
 
-p.Plot3D(deck.NbImage, speckle.List_Sheets, Liste_Projection, deck.WingFrame)
+p.Plot3D(deck.NbImage, speckle.List_Sheets, Liste_Projection, deck.WingFrame)#Plot in 3D the loaded speckle and the anamorphosed
 
-p.PlotUnfolded(deck.NbImage, speckle.List_Sheets, List_Unfolded[0], WingFrameUnfolded, yf, zf)
+p.PlotUnfolded(deck.NbImage, speckle.List_Sheets, List_Unfolded[0], WingFrameUnfolded, yf, zf)#Plot the unfolded speckle
 
 ##-----------------------------FIN AFFICHAGE---------------------------------##
 
