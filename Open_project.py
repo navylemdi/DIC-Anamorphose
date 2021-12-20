14ada3c00705faae0a73b54a797f93c89d2a74a8
navylemdi <84194324+navylemdi@users.noreply.github.com>
2021-12-19 19:08:42 -0500

    Update Open_project.py

--- Open_project.py
+++ Open_project.py
@@ -1,7 +1,7 @@
 from Module import *
 
 p = Project()
-p.open(r'/Users/yvan/Desktop/ETS_montreal/Cours/E21/MTR892 - Projet technique/AnamorphosePlane/New_project.yaml')
+p.open(r'./New_project.yaml')
 p.PlotReference()
 p.Plot3D()
 p.PlotUnfolded()
